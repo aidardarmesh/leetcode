0e926dc02e180c97806fb193de600f15402e4119
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-25 21:07:35 +0600

    Sliding sum is 10x faster

--- Problems/643. Maximum Average Subarray I.py
+++ Problems/643. Maximum Average Subarray I.py
@@ -3,24 +3,22 @@ from typing import *
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         n = len(nums)
-        # solve it with prefix-sum
-        # it already should have first element in it
-        prefix_sum = []
-        prefix_sum.append(nums[0])
+        sum_val = 0
 
-        for i in range(0, n-1):
-            prefix_sum.append(prefix_sum[i] + nums[i+1])
+        for i in range(0, k):
+            sum_val += nums[i]
+
+        max_val = sum_val
         
-        max_sum = prefix_sum[k-1]
+        for i in range(0, n-k):
+            sum_val = sum_val - nums[i] + nums[i+k]
 
-        for i in range(k, n):
-            k_sum = prefix_sum[i] - prefix_sum[i-k]
+            if sum_val > max_val:
+                max_val = sum_val
 
-            if k_sum > max_sum:
-                max_sum = k_sum
-        
-        return max_sum / k
+        return max_val / k
 
 s = Solution()
 
-assert s.findMaxAverage([1,12,-5,-6,50,3], 4) == 12.75
+assert s.findMaxAverage([1,12,-5,-6,50,3,7,8], 3) == 20.0
+assert s.findMaxAverage([8], 1) == 8.0

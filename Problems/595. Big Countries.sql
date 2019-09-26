3a5cce10fa17a8528c849b8a582ad822c27ecd57
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-26 13:19:08 +0600

    UNION solution works little bit faster

--- Problems/595. Big Countries.sql
+++ Problems/595. Big Countries.sql
@@ -3,5 +3,14 @@ SELECT
 FROM 
     world
 WHERE
-    area > 3000000 OR population > 25000000
+    area > 3000000
+    
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population > 25000000
 ;

2da6a916e88893171fab6b84ec47587c46e80055
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-28 10:42:15 +0600

    Two pointers solution

--- Problems/977. Squares of a Sorted Array.py
+++ Problems/977. Squares of a Sorted Array.py
@@ -3,7 +3,32 @@ import heapq
 
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
-        return sorted([a**2 for a in A])
+        n = len(A)
+        squares = []
+        j = 0 # positive part pointer
+
+        while j < n and A[j] < 0:
+            j += 1
+        
+        i = j - 1 # negative part pointer
+
+        while i >= 0 and j < n:
+            if A[i] ** 2 < A[j] ** 2:
+                squares.append(A[i]**2)
+                i -= 1
+            else:
+                squares.append(A[j]**2)
+                j += 1
+        
+        while i >= 0:
+            squares.append(A[i]**2)
+            i -= 1
+        
+        while j < n:
+            squares.append(A[j]**2)
+            j += 1
+
+        return squares
 
 s = Solution()
 

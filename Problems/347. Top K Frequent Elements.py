bba838eac6e666caf49a8fc7ea3fb13c4379f377
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-28 13:17:56 +0600

    Solution using nlargest and comparison key

--- Problems/347. Top K Frequent Elements.py
+++ Problems/347. Top K Frequent Elements.py
@@ -1,28 +1,10 @@
 from typing import *
-import heapq
+import heapq, collections
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_freq = {}
-        heap = []
-        most_freq = []
-
-        for num in nums:
-            if num in num_freq:
-                num_freq[num] += 1
-            else:
-                num_freq[num] = 1
-
-        for i in num_freq:
-            if len(heap) == k:
-                heapq.heappushpop(heap, (num_freq[i], i))
-            else:
-                heapq.heappush(heap, (num_freq[i], i))
-
-        for i in range(len(heap)):
-            most_freq.append(heapq.heappop(heap)[1])
-
-        return most_freq
+        counts = collections.Counter(nums)
+        return heapq.nlargest(k, counts.keys(), key=counts.get)
 
 s = Solution()
 

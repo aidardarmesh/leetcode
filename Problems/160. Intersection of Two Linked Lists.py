b84301bc4f1e8a3af5909ae48e08b34c5ff644a8
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-09-30 14:58:00 +0600

    Naive try

---
+++ Problems/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,66 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        curA = headA
+
+        while curA != None:
+            # print("curA is", curA.val)
+            curB = headB
+
+            while curB != None:
+                # print("curB is", curB.val)
+                if curA == curB:
+                    return curB
+
+                curB = curB.next
+            
+            curA = curA.next
+        
+        return None
+
+# nodeA_2 = ListNode(2)
+# nodeA_6 = ListNode(6)
+# nodeA_4 = ListNode(4)
+# nodeA_2.next = nodeA_6
+# nodeA_6.next = nodeA_4
+
+# nodeB_1 = ListNode(1)
+# nodeB_5 = ListNode(5)
+# nodeB_1.next = nodeB_5
+
+# s = Solution()
+
+# intersection = s.getIntersectionNode(nodeA_2, nodeB_1)
+# print(intersection.val)
+
+# nodeC_8 = ListNode(8)
+# nodeC_4 = ListNode(4)
+# nodeC_5 = ListNode(5)
+# nodeC_8.next = nodeC_4
+# nodeC_4.next = nodeC_5
+
+# nodeA_4 = ListNode(4)
+# nodeA_1 = ListNode(1)
+# nodeA_4.next = nodeA_1
+# nodeA_1.next = nodeC_8
+
+# nodeB_5 = ListNode(5)
+# nodeB_0 = ListNode(0)
+# nodeB_1 = ListNode(1)
+# nodeB_5.next = nodeB_0
+# nodeB_0.next = nodeB_1
+# nodeB_1.next = nodeC_8
+
+# s = Solution()
+
+# intersection = s.getIntersectionNode(nodeA_4, nodeB_5)
+# print(intersection.val)

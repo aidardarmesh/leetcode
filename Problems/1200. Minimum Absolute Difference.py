e1f7fe80ed9331bf48141f4523703846f12c0cb3
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-01 22:21:31 +0600

    Naive solution

---
+++ Problems/1200. Minimum Absolute Difference.py
@@ -0,0 +1,24 @@
+from typing import *
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        min_delta = 9999999
+        result = []
+        n = len(arr)
+        arr = sorted(arr)
+
+        for i in range(0, n-1):
+            delta = arr[i+1] - arr[i]
+            min_delta = min(min_delta, delta)
+        
+        for i in range(0, n-1):
+            if arr[i+1] - arr[i] == min_delta:
+                result.append([arr[i], arr[i+1]])
+        
+        return result
+
+s = Solution()
+
+assert s.minimumAbsDifference([4,2,1,3]) == [[1,2],[2,3],[3,4]]
+assert s.minimumAbsDifference([1,3,6,10,15]) == [[1,3]]
+assert s.minimumAbsDifference([3,8,-10,23,19,-4,-14,27]) == [[-14,-10],[19,23],[23,27]]

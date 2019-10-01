b3c6b07d2f427e6550e492cf4c4ff52abc3bfda7
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-01 12:45:40 +0600

    Iterative with stack'

---
+++ Problems/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,34 @@
+from typing import *
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        node = root
+        
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            elif stack:
+                node = stack.pop()
+                res.append(node.val)
+                node = node.right
+        
+        return res
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node1.right = node2
+node2.left = node3
+
+s = Solution()
+print(s.inorderTraversal(node1))

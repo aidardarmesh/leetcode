2ccdfe7fdf4e9692d29c6ced3580fc4ae450c47e
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-02 22:28:30 +0600

    Actual solution

--- Problems/697. Degree of an Array.py
+++ Problems/697. Degree of an Array.py
@@ -3,21 +3,24 @@ import collections
 
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        first, count, degree, res = {}, {}, 0, 0
+        left, right, count = {}, {}, {}
 
         for i, num in enumerate(nums):
-            first.setdefault(num, i)
+            left.setdefault(num, i)
+            right[num] = i
             count[num] = count.get(num, 0) + 1
+        
+        res = len(nums)
+        degree = max(count.values())
 
-            if count[num] > degree:
-                degree = count[num]
-                res = i - first[num] + 1
-            elif count[num] == degree:
-                res = min(res, i - first[num] + 1)
-
+        for num in left:
+            if count[num] == degree:
+                res = min(res, right[num] - left[num] + 1)
+        
         return res
 
 s = Solution()
 
+assert s.findShortestSubArray([1, 2, 3]) == 1
 assert s.findShortestSubArray([1, 2, 2, 3, 1]) == 2
 assert s.findShortestSubArray([1,2,2,3,1,4,2]) == 6

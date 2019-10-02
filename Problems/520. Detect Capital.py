b7ad4720ef1fd0053d4b8a31d360610509974ae6
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-02 22:49:35 +0600

    Using built-in func-s

--- Problems/520. Detect Capital.py
+++ Problems/520. Detect Capital.py
@@ -2,21 +2,7 @@ from typing import *
 
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        upper, lower, first_upper, n = 0, 0, False, len(word)
-
-        for i, char in enumerate(word):
-            if "A" <= char and char <= "Z":
-                if i == 0:
-                    first_upper = True
-                
-                upper += 1
-            elif "a" <= char and char <= "z":
-                lower += 1
-        
-        if lower == n or upper == n or (first_upper and upper == 1):
-            return True
-        
-        return False
+        return word.islower() or word.isupper() or word.istitle()
 
 s = Solution()
 

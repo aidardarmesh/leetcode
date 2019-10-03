b73aa0ca4aed0fd32869b8f05d71ab2b5a0c4fd1
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-03 21:13:58 +0600

    Used bin() to convert from int to binary string with literal 0b

--- Problems/67. Add Binary.py
+++ Problems/67. Add Binary.py
@@ -2,32 +2,7 @@ from typing import *
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res, int_, rem = [], 0, 0
-        a = list(map(int, a))
-        b = list(map(int, b))
-
-        while a and b:
-            sum = a.pop() + b.pop() + int_
-            int_ = sum // 2
-            rem = sum % 2
-            res.insert(0, rem)
-        
-        while a:
-            sum = a.pop() + int_
-            int_ = sum // 2
-            rem = sum % 2
-            res.insert(0, rem)
-        
-        while b:
-            sum = b.pop() + int_
-            int_ = sum // 2
-            rem = sum % 2
-            res.insert(0, rem)
-        
-        if int_ != 0:
-            res.insert(0, int_)
-        
-        return "".join(list(map(str, res)))
+        return bin(int(a,2) + int(b,2))[2:]
 
 s = Solution()
 

9db0d1e43d82a8904ecae794c96020bab1ca2512
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-04 21:31:50 +0600

    Used primes to get unique ids by multiplication

--- Problems/49. Group Anagrams.py
+++ Problems/49. Group Anagrams.py
@@ -3,9 +3,14 @@ from typing import *
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
+        # multiplication of primes gives unrepeatable results
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
         
         for s in strs:
-            id = "".join(sorted(s))
+            id = 1
+
+            for ch in s:
+                id *= primes[ord(ch) - ord('a')]
             
             if not id in d:
                 d[id] = []

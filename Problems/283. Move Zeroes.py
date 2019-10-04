3d9d1e629a95fcfe95bdabffaf0a6cb2b2020d28
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-04 23:05:05 +0600

    lastNonZeroIndex nice solution

--- Problems/283. Move Zeroes.py
+++ Problems/283. Move Zeroes.py
@@ -2,16 +2,15 @@ from typing import *
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        zeros = 0
+        last_non_zero_index = 0
 
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] == 0:
-                del nums[i]
-                zeros += 1
-
-        while zeros > 0:
-            nums.append(0)
-            zeros -= 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero_index] = nums[i]
+                last_non_zero_index += 1
+        
+        for i in range(last_non_zero_index, len(nums)):
+            nums[i] = 0
 
 a = [0,0,1]
 
@@ -20,3 +19,5 @@ s = Solution()
 s.moveZeroes(a)
 
 print(a)
+
+[0,1,0,3,12]

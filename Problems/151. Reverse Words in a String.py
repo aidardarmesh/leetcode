79566992249ed916860069f672946c310a294037
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-05 13:12:26 +0600

    Used built-in func-s

---
+++ Problems/151. Reverse Words in a String.py
@@ -0,0 +1,11 @@
+from typing import *
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
+
+s = Solution()
+
+assert s.reverseWords("the sky is blue") == "blue is sky the"
+assert s.reverseWords("  hello world!  ") == "world! hello"
+assert s.reverseWords("a good   example") == "example good a"

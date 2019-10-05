de86a8c866e0a0b26b58236551b527127503e158
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-05 13:14:33 +0600

    Used [::-1] instead of reversed

--- Problems/151. Reverse Words in a String.py
+++ Problems/151. Reverse Words in a String.py
@@ -2,7 +2,7 @@ from typing import *
 
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return " ".join(reversed(s.split()))
+        return " ".join(s.split()[::-1])
 
 s = Solution()
 

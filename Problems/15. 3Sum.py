984968a19d3b6737adb7a54293ef95705b9a3e89
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-06 14:21:25 +0600

    Correct solution, 3sum becomes 2sum if a+b=-c

--- Problems/15. 3Sum.py
+++ Problems/15. 3Sum.py
@@ -6,19 +6,22 @@ class Solution:
         nums.sort()
 
         for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
             l = i+1
             r = n-1
 
             while l < r:
-                sum2 = nums[l] + nums[r]
-                sum3 = nums[i] + sum2
+                target = nums[i]*(-1)
 
-                if sum3 < 0:
+                if nums[l] + nums[r] == target:
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
-                elif sum3 == 0:
-                    if [nums[i], nums[l], nums[r]] not in res:
-                        res.append([nums[i], nums[l], nums[r]])
 
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                elif nums[l] + nums[r] < target:
                     l += 1
                 else:
                     r -= 1
@@ -26,6 +29,7 @@ class Solution:
         return res
 
 s = Solution()
+
 assert s.threeSum([-1, 0, 1, 2, -1, -4]) == [
     [-1, -1, 2],
     [-1, 0, 1]

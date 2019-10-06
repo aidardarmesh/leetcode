46f35e71890bfbefcaac90d0c93178b134a7ae1a
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-06 13:56:22 +0600

    Correct, but TL

--- Problems/15. 3Sum.py
+++ Problems/15. 3Sum.py
@@ -2,37 +2,34 @@ from typing import *
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        l, r, res = 0, len(nums)-1, []
+        n, res = len(nums), []
         nums.sort()
 
-        while l < r:
-            sum2 = nums[l] + nums[r]
-            min_diff = 99999
-            closest = 99999
+        for i in range(n-2):
+            l = i+1
+            r = n-1
 
-            for i in range(l+1, r):
-                sum3 = sum2 + nums[i]
+            while l < r:
+                sum2 = nums[l] + nums[r]
+                sum3 = nums[i] + sum2
 
-                if sum3 == 0:
-                    if [nums[l], nums[i], nums[r]] not in res:
-                        res.append([nums[l], nums[i], nums[r]])
-                elif abs(sum3) < min_diff:
-                    min_diff = abs(sum3)
-                    closest = sum3
-            
-            if closest < 0:
-                l += 1
-            else:
-                r -= 1
+                if sum3 < 0:
+                    l += 1
+                elif sum3 == 0:
+                    if [nums[i], nums[l], nums[r]] not in res:
+                        res.append([nums[i], nums[l], nums[r]])
+
+                    l += 1
+                else:
+                    r -= 1
         
         return res
 
 s = Solution()
-print(s.threeSum([-1, 0, 1, 2, -1, -4]))
 assert s.threeSum([-1, 0, 1, 2, -1, -4]) == [
-    [-1, 0, 1],
-    [-1, -1, 2]
+    [-1, -1, 2],
+    [-1, 0, 1]
 ]
-
-# -4 -1 -1 0 1 2
-# l = -4, r = 2 => sum = -2, go from l to r exclusively to find t
+assert s.threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]) == [
+    [-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]
+]

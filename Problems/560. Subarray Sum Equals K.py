1843dfaef27d538fe423a70177962d3b53feac56
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-07 00:17:53 +0600

    58/80 TL

--- Problems/560. Subarray Sum Equals K.py
+++ Problems/560. Subarray Sum Equals K.py
@@ -8,12 +8,11 @@ class Solution:
         for i in range(1, n):
             prefix.append(nums[i] + prefix[i-1])
         
-        for i in range(n-1, -1, -1):
-            for j in range(i-1, -1, -1):
-                if prefix[i] == k:
-                    counter += 1
-                
-                if prefix[i] - prefix[j] == k:
+        prefix.append(0)
+        
+        for i in range(-1, n-1):
+            for j in range(i+1, n):
+                if prefix[j] - prefix[i] == k:
                     counter += 1
         
         return counter
@@ -21,4 +20,4 @@ class Solution:
 s = Solution()
 
 assert s.subarraySum([1,1,1], 2) == 2
-assert s.subarraySum([1,2,3,4,5,6,7,1,23,21,3,1,2,1,1,1,1,1,12,2,3,2,3,2,2]) == 5
+assert s.subarraySum([1,2,3,4,5,6,7,1,23,21,3,1,2,1,1,1,1,1,12,2,3,2,3,2,2], 6) == 5

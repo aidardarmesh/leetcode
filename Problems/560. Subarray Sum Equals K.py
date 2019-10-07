48e0f7b5b732ef71ee8c8e117b585e6f6121973c
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-07 12:28:18 +0600

    Correct solution

--- Problems/560. Subarray Sum Equals K.py
+++ Problems/560. Subarray Sum Equals K.py
@@ -8,7 +8,7 @@ class Solution:
             sum += nums[i]
 
             if sum-k in freq:
-                count += 1
+                count += freq[sum-k]
             
             freq[sum] = freq.get(sum, 0) + 1
         
@@ -18,3 +18,4 @@ s = Solution()
 
 assert s.subarraySum([1,1,1], 2) == 2
 assert s.subarraySum([1,2,3,4,5,6,7,1,23,21,3,1,2,1,1,1,1,1,12,2,3,2,3,2,2], 6) == 5
+assert s.subarraySum([0,0,0,0,0,0,0,0,0,0], 0) == 55

0e9205d580e4edffaf85dce918350a7f8f6d65f2
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-07 21:28:39 +0600

    Better recursion code

--- Problems/112. Path Sum.py
+++ Problems/112. Path Sum.py
@@ -11,12 +11,13 @@ class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
             return False
-        1
-        if not root.left and not root.right and sum == root.val:
-            return True
+
+        sum -= root.val
         
-        return self.hasPathSum(root.left, sum-root.val) \
-            or self.hasPathSum(root.right, sum-root.val)
+        if not root.left and not root.right:
+            return sum == 0
+        
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
 
 s = Solution()
 

0ff595979fd2059422053c4a9a0b1bf8765d3e87
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-13 12:26:45 +0600

    Naive solution

---
+++ Problems/231. Power of Two.py
@@ -0,0 +1,20 @@
+from typing import *
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            
+            n = n // 2
+        
+        return True
+
+s = Solution()
+
+assert s.isPowerOfTwo(1) == True
+assert s.isPowerOfTwo(16) == True
+assert s.isPowerOfTwo(218) == False

8d6c2c4cd99e83773c983e47b7679bf887c053dc
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-14 12:34:42 +0600

    Shorter and more logical solution

--- Problems/628. Maximum Product of Three Numbers.py
+++ Problems/628. Maximum Product of Three Numbers.py
@@ -2,17 +2,10 @@ from typing import *
 
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        max, n = nums[0]*nums[1]*nums[2], len(nums)
+        nums.sort()
+        n = len(nums)
 
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    prod = nums[i] * nums[j] * nums[k]
-        
-                    if max < prod:
-                        max = prod
-        
-        return max
+        return max(nums[0]*nums[1]*nums[n-1], nums[n-3]*nums[n-2]*nums[n-1])
 
 s = Solution()
 

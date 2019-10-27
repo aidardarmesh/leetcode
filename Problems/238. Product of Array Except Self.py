de2e8bfe920a7429b7054ebd1fa58d9d91f02220
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-28 00:00:33 +0600

    Optimized, two iterations soltuion

--- Problems/238. Product of Array Except Self.py
+++ Problems/238. Product of Array Except Self.py
@@ -1,22 +1,27 @@
 from typing import *
 
 class Solution:
-    def product(self, nums: List[int]) -> int:
-        prod = 1
-
-        for num in nums:
-            prod *= num
-        
-        return prod
-    
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = []
+        output, prod = [], 1
 
         for i in range(len(nums)):
-            res.append(self.product(nums[:i] + nums[i+1:]))
+            output.append(prod)
+            prod *= nums[i]
         
-        return res
+        prod = 1
+
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= prod
+            prod *= nums[i]
+        
+        return output
+
 
 s = Solution()
 
 assert s.productExceptSelf([1,2,3,4]) == [24,12,8,6]
+assert s.productExceptSelf([1,2]) == [2,1]
+assert s.productExceptSelf([1,0]) == [0,1]
+assert s.productExceptSelf([0,0]) == [0,0]
+assert s.productExceptSelf([-1,-1,-1,-1]) == [-1,-1,-1,-1]
+assert s.productExceptSelf([-1,2,-3,4]) == [-24,12,-8,6]

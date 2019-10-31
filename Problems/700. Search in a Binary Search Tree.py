786968a9237df8fccf08bd9a6f6b5cf6013293a9
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-31 13:51:46 +0600

    Naive BST search

---
+++ Problems/700. Search in a Binary Search Tree.py
@@ -0,0 +1,20 @@
+from typing import *
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        while root:
+            if root.val == val:
+                return root
+            elif root.val < val:
+                root = root.right
+            else:
+                root = root.left
+        
+        return None

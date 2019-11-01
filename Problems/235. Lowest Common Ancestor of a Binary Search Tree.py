3c968ea60c1c8285ecdc6c1fa70906cc2ed29ff1
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-01 17:51:41 +0600

    Author solution. How genius it is

---
+++ Problems/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,18 @@
+from typing import *
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        '''lca is split point (node where p and q cannot be in same subtree)'''
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root

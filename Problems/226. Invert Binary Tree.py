b32c1bd85a3ecba3ac9e5d3d3469459be9dc2571
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-05 12:17:17 +0600

    Naive solution

---
+++ Problems/226. Invert Binary Tree.py
@@ -0,0 +1,20 @@
+from typing import *
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+        
+        root.left, root.right = root.right, root.left
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root

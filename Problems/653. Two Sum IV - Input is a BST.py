80a9910d8864d45ec551fe77a28f126fec502386
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-07 22:44:00 +0600

    Speed-up, but not best possible

--- Problems/653. Two Sum IV - Input is a BST.py
+++ Problems/653. Two Sum IV - Input is a BST.py
@@ -9,32 +9,24 @@ from typing import *
 
 class Solution(object):
     def findTarget(self, root, k):
-        def in_order(root):
-            stack = []
-            node = root
-            res = []
-            
-            while stack or node:
-                if node:
-                    stack.append(node)
-                    node = node.left
-                elif stack:
-                    node = stack.pop()
-                    res.append(node.val)
-                    node = node.right
-            
-            return res
+        if not root:
+            return False
+
+        stack = []
+        node = root
+        visited = set()
         
-        nums = in_order(root)
-        l, r = 0, len(nums)-1
-        
-        while l < r:
-            pair_sum = nums[l]+nums[r]
-            if pair_sum == k:
-                return True
-            elif pair_sum > k:
-                r -= 1
-            else:
-                l += 1
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            elif stack:
+                node = stack.pop()
+
+                if k-node.val in visited:
+                    return True
+                
+                visited.add(node.val)
+                node = node.right
         
         return False

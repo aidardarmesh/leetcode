43c6ada6c5516b0ae6b1033278b54ee9cb037cda
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-12 19:01:53 +0600

    Naive solution

---
+++ Problems/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,56 @@
+from typing import *
+
+
+class Node:
+    
+    def __init__(self, val):
+        self.val = val
+        self.is_word = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node('')
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        cur = self.root
+        
+        for c in word:
+            if not c in cur.children:
+                cur.children[c] = Node(c)
+            cur = cur.children[c]
+        
+        cur.is_word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        cur = self.root
+        
+        for c in word:
+            if not c in cur.children:
+                return False
+            cur = cur.children[c]
+        
+        return cur.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        cur = self.root
+        
+        for c in prefix:
+            if not c in cur.children:
+                return False
+            cur = cur.children[c]
+        
+        return True

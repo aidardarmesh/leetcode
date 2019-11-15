465f4c5554580ed46133c50cfbc269a87185bb31
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-15 18:18:00 +0600

    Naive List solution again. Did not pass all tests

---
+++ Problems/706. Design HashMap.py
@@ -0,0 +1,65 @@
+from typing import *
+
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.n = 1_000
+        self.buckets = [[] for _ in range(self.n)]  # bucket is list and elem is bucket is also list
+    
+    def _hash(self, key):
+        return key % self.n
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        idx = self._hash(key)
+
+        for elem in self.buckets[idx]:
+            if elem[0] == key:
+                elem[1] = value
+                return
+        
+        self.buckets[idx].append([key, value])
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        idx = self._hash(key)
+
+        for elem in self.buckets[idx]:
+            if elem[0] == key:
+                return elem[1]
+        
+        return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        idx = self._hash(key)
+        cnt = -1
+
+        for elem in self.buckets[idx]:
+            if elem[0] == key:
+                cnt += 1
+                break
+
+        if cnt != -1:
+            self.buckets[idx].pop(cnt)
+
+h_map = MyHashMap()
+
+h_map.put(1,1)
+h_map.put(2,2)
+assert h_map.get(1) == 1
+assert h_map.get(3) == -1
+h_map.put(2,1)
+assert h_map.get(2) == 1
+h_map.remove(2)
+assert h_map.get(2) == -1

c27449abb5331813aa445feadc738cfb78224c9b
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-17 21:32:06 +0600

    Correct solution

--- Problems/49. Group Anagrams.py
+++ Problems/49. Group Anagrams.py
@@ -3,21 +3,16 @@ from typing import *
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
-        # multiplication of primes gives unrepeatable results
-        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
         
-        for s in strs:
-            id = 1
-
-            for ch in s:
-                id *= primes[ord(ch) - ord('a')]
+        for str_ in strs:
+            key = ''.join(sorted(str_))
             
-            if not id in d:
-                d[id] = []
-            
-            d[id].append(s)
+            if key in d:
+                d[key].append(str_)
+            else:
+                d[key] = [str_]
         
-        return [d[id] for id in d]
+        return d.values()
 
 s = Solution()
 

6a4cde2883fe78a3be6a8e8333ab4189863050a7
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-21 17:58:09 +0600

    Tried to figure out template 2 def

--- Problems/162. Find Peak Element.py
+++ Problems/162. Find Peak Element.py
@@ -1,6 +1,11 @@
 from typing import *
 
 class Solution:
+    '''
+    Main strategy in BS template 2 is to half search space
+    until search space is sloped to zero and return left pointer
+    in the end
+    '''
     def findPeakElement(self, nums: List[int]) -> int:
         l, r = 0, len(nums)-1
         

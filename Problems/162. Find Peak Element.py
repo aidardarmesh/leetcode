5037a472f5432ddde3c80c3b662bd25ffceb4de8
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-21 17:54:30 +0600

    Correct solution. If right is larger, go right until end. End is peak. Else, go left. You will come to first mid point

--- Problems/162. Find Peak Element.py
+++ Problems/162. Find Peak Element.py
@@ -2,25 +2,14 @@ from typing import *
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        def get_item(nums, i):
-            if i < 0 or i >= len(nums):
-                return float('-inf')
-            
-            return nums[i]
-        
         l, r = 0, len(nums)-1
         
         while l < r:
             m = (l+r) // 2
             
-            if get_item(nums, m) < get_item(nums, m+1):
-                l = m+1
-                continue
-            
-            if get_item(nums, m) < get_item(nums, m-1):
+            if nums[m] > nums[m+1]:
                 r = m
-                continue
-            
-            return m
+            else:
+                l = m+1
         
-        return -1
+        return l

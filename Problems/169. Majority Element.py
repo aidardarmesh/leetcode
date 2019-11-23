77c08ba60c009d4b848100d6760b0567e76098ac
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-23 23:56:27 +0600

    Bit manipulation solution

--- Problems/169. Majority Element.py
+++ Problems/169. Majority Element.py
@@ -2,6 +2,18 @@ from typing import *
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
+        candidate = 0
         
-        return nums[len(nums)//2]
+        for i in range(32):
+            ones = 0
+            
+            for num in nums:
+                ones += (num & (1 << i)) != 0
+            
+            if ones > len(nums)-ones:
+                if i == 31:
+                    candidate = -((1<<31)-candidate)
+                else:
+                    candidate |= 1 << i
+        
+        return candidate

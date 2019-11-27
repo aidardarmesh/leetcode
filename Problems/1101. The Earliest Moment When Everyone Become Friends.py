da3209072d92cc6c0a786f220bd1928e426847a6
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-27 17:34:48 +0600

    Rank didn't give speed at all. Looks like waste when path compression is used

--- Problems/1101. The Earliest Moment When Everyone Become Friends.py
+++ Problems/1101. The Earliest Moment When Everyone Become Friends.py
@@ -4,10 +4,12 @@ class DSU:
     def __init__(self, n):
         self.n = n
         self.parent = {}
+        self.rank = {}
         self.groups = n
     
     def make_set(self, v):
         self.parent[v] = v
+        self.rank[v] = 0
     
     def find_set(self, v):
         if v == self.parent[v]:
@@ -23,7 +25,14 @@ class DSU:
         if u_root == v_root:
             return
         
-        self.parent[u_root] = v_root
+        if self.rank[u_root] < self.rank[v_root]:
+            self.parent[u_root] = v_root
+        elif self.rank[u_root] > self.rank[v_root]:
+            self.parent[v_root] = u_root
+        else:
+            self.parent[u_root] = v_root
+            self.rank[v_root] = self.rank[u_root] + 1
+
         self.groups -= 1
 
 class Solution:

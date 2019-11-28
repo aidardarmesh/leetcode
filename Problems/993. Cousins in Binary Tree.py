ba4ccb3bf9ea42a2601ddae0019e5a97b2926d0c
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-28 22:12:17 +0600

    Naive BFS

---
+++ Problems/993. Cousins in Binary Tree.py
@@ -0,0 +1,38 @@
+from typing import *
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        if not root:
+            return False
+        
+        parent = {}
+        queue = [root]
+        
+        while queue:
+            size = len(queue)
+            level = []
+            
+            for _ in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                    parent[node.left.val] = node.val
+                
+                if node.right:
+                    queue.append(node.right)
+                    parent[node.right.val] = node.val
+            
+            if x in level and y in level:
+                if x in parent and y in parent and parent[x] != parent[y]:
+                    return True
+        
+        return False

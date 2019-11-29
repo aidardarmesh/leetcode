6c7d7e7a84ba6af414955fea82f5f2730330d65f
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-29 15:46:14 +0600

    BFS. Solution

--- Problems/200. Number of Islands.py
+++ Problems/200. Number of Islands.py
@@ -2,46 +2,31 @@ from typing import *
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        Strategy is to "draw" lands in water
+        '''
         if not grid:
             return 0
         
         N = len(grid)
         M = len(grid[0])
-        
-        islands = self.lands = 0
-        visited = {}
+        islands = 0
         
         for i in range(N):
             for j in range(M):
                 if grid[i][j] == '1':
-                    visited[(i,j)] = False
-                    self.lands += 1
-        
-        def get_unvisited():
-            for key, val in visited.items():
-                if not val:
-                    return key
-            
-            return None
-        
-        def bfs(s):
-            queue = [s]
-            visited[s] = True
-            self.lands -= 1
-            
-            while queue:
-                i,j = queue.pop(0)
-                
-                for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
-                    if 0 <= x < N and 0 <= y < M:
-                        if grid[x][y] == '1' and not visited[(x,y)]:
-                            visited[(x,y)] = True
-                            self.lands -= 1
-                            queue.append((x,y))
-        
-        while self.lands:
-            bfs(get_unvisited())
-            islands += 1
+                    grid[i][j] = '0'
+                    islands += 1
+                    queue = [(i,j)]
+                    
+                    while queue:
+                        x,y = queue.pop()
+                        
+                        for ni, nj in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                            if 0 <= ni < N and 0 <= nj < M:
+                                if grid[ni][nj] == '1':
+                                    grid[ni][nj] = '0'
+                                    queue.append((ni,nj))
         
         return islands
 

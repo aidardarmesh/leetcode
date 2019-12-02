ce61de6d018d7a322c04070416d699026ded8a98
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-02 21:08:43 +0600

    Better written

--- Problems/695. Max Area of Island.py
+++ Problems/695. Max Area of Island.py
@@ -6,18 +6,12 @@ class Solution:
         M = len(grid[0])
         max_ = 0
         
-        def dfs(x, y):
-            if not grid[x][y]:
-                return 0
+        def dfs(x,y):
+            if 0 <= x < N and 0 <= y < M and grid[x][y]:
+                grid[x][y] = 0
+                return 1 + dfs(x-1,y) + dfs(x+1,y) + dfs(x,y-1) + dfs(x,y+1)
             
-            area = 1
-            grid[x][y] = 0
-            
-            for ni, nj in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]:
-                if 0 <= ni < N and 0 <= nj < M and grid[ni][nj]:
-                    area += dfs(ni, nj)
-            
-            return area
+            return 0
         
         for i in range(N):
             for j in range(M):

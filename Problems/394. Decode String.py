e155609e2364ca403829356a59b4f3d972bd6bba
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-03 11:08:42 +0600

    One more different implementatino

--- Problems/394. Decode String.py
+++ Problems/394. Decode String.py
@@ -3,19 +3,19 @@ from typing import *
 class Solution:
     def decodeString(self, s: str) -> str:
         stack, cur_str, cur_num = [], '', ''
-
+        
         for c in s:
-            if c == '[':
+            if c.isdigit():
+                cur_num += c
+            elif c == '[':
                 stack.append(cur_str)
                 stack.append(cur_num)
-                cur_num, cur_str = '', ''
+                cur_str = ''
+                cur_num = ''
             elif c == ']':
-                cur_num = int(stack.pop())
-                cur_str = cur_num * cur_str
+                cur_str = int(stack.pop()) * cur_str
                 cur_str = stack.pop() + cur_str
                 cur_num = ''
-            elif c.isdigit():
-                cur_num += c
             else:
                 cur_str += c
         

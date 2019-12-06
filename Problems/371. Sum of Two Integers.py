c7ab1321a35b1e181a37a0e2f972a1fdb90e68cb
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-06 09:18:43 +0600

    Discussion solution. Got idea to sum according to mask

---
+++ Problems/371. Sum of Two Integers.py
@@ -0,0 +1,11 @@
+from typing import *
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MAX = 0x7FFFFFFF
+        mask = 0xFFFFFFFF
+        
+        while b:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        
+        return a if a <= MAX else (~a) ^ mask

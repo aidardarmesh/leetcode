b29be23da0b87e2d831ca101ffe1f83e4a4a717b
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-06 14:27:13 +0600

    Implemented Recursively

--- Problems/344. Reverse String.py
+++ Problems/344. Reverse String.py
@@ -2,15 +2,13 @@ from typing import *
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        l, r = 0, len(s)-1
+        def helper(left, right, s):
+            if left <= right:
+                s[left], s[right] = s[right], s[left]
+                helper(left+1, right-1, s)
+        
+        helper(0, len(s)-1, s)
 
-        while l <= r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
 
 s = Solution()
 l = list('hello')

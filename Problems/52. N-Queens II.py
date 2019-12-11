a3757cc3528eef890ed6fe168e0e0476beb0d70d
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-11 16:15:17 +0600

    Rewrote

--- Problems/52. N-Queens II.py
+++ Problems/52. N-Queens II.py
@@ -2,32 +2,33 @@ from typing import *
 
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        def is_not_under_attack(row, col):
-            return not (rows[col] or hills[row - col] or dales[row + col])
+        cols = [0 for _ in range(n)]
+        hills = [0 for _ in range(2*n-1)]
+        dales = [0 for _ in range(2*n-1)]
+
+        def is_not_attacked(row, col):
+            return not (cols[col] or hills[row + col] or dales[row - col])
         
         def place_queen(row, col):
-            rows[col] = 1
-            hills[row - col] = 1  # "hill" diagonals
-            dales[row + col] = 1  # "dale" diagonals
+            cols[col] = 1
+            hills[row + col] = 1
+            dales[row - col] = 1
         
         def remove_queen(row, col):
-            rows[col] = 0
-            hills[row - col] = 0  # "hill" diagonals
-            dales[row + col] = 0  # "dale" diagonals
-        
-        def backtrack(row = 0, count = 0):
+            cols[col] = 0
+            hills[row + col] = 0
+            dales[row - col] = 0
+
+        def backtrack(row, count):
             for col in range(n):
-                if is_not_under_attack(row, col):
-                    place_queen(row, col)
-                    if row + 1 == n:
+                if is_not_attacked(row, col):
+                    if row == n-1:
                         count += 1
                     else:
+                        place_queen(row, col)
                         count = backtrack(row + 1, count)
-                    remove_queen(row, col)
+                        remove_queen(row, col)
+            
             return count
         
-        rows = [0] * n
-        hills = [0] * (2 * n - 1)  # "hill" diagonals
-        dales = [0] * (2 * n - 1)  # "dale" diagonals
-        return backtrack()
-        
+        return backtrack(0, 0)

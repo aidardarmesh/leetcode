c574111ad974b175ef7b7835eedfd6849730ce48
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-12 16:01:06 +0600

    Unfolded recursion

--- Problems/100. Same Tree.py
+++ Problems/100. Same Tree.py
@@ -10,14 +10,28 @@ from collections import deque
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q:
+        def check(p, q):
+            if not p and not q:
+                return True
+            
+            if not p or not q:
+                return False
+            
+            if p.val != q.val:
+                return False
+            
             return True
         
-        if not p or not q:
-            return False
-        
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and \
-               self.isSameTree(p.right, q.right)
+        queue = [(p, q)]
+
+        while queue:
+            p, q = queue.pop(0)
+
+            if not check(p, q):
+                return False
+            
+            if p:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+            
+        return True

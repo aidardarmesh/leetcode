17f27ff33c6a4600d18ef819b6e3da9a1111fbb0
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-12 17:43:08 +0600

    Recalled recursive approach. Need to make iterative

--- Problems/22. Generate Parentheses.py
+++ Problems/22. Generate Parentheses.py
@@ -1,23 +1,22 @@
 from typing import *
 
 class Solution:
-    def __init__(self):
-        self.parens = []
-    
-    def generate(self, cur, n_open, n_closed, n):
-        if len(cur) == 2*n:
-            self.parens.append(cur)
-        
-        if n_open < n:
-            self.generate(cur+"(", n_open+1, n_closed, n)
-        
-        if n_closed < n_open:
-            self.generate(cur+")", n_open, n_closed+1, n)
-
     def generateParenthesis(self, n: int) -> List[str]:
-        self.generate("(", 1, 0, n)
-
-        return self.parens
+        res = []
+        
+        def helper(s, n_open, n_closed):
+            if n_open < n:
+                helper(s+'(', n_open+1, n_closed)
+            
+            if n_closed < n_open:
+                helper(s+')', n_open, n_closed+1)
+            
+            if n_closed == n:
+                res.append(s)
+        
+        helper('', 0, 0)
+        
+        return res
 
 s = Solution()
 

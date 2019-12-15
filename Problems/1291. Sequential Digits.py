e6464c4d9fa52af6d1bf5d08be94c96e65f6df48
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-15 15:38:22 +0600

    BRUTE FORCE

---
+++ Problems/1291. Sequential Digits.py
@@ -0,0 +1,49 @@
+from typing import *
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        s = [
+            12,
+            23,
+            34,
+            45,
+            56,
+            67,
+            78,
+            89,
+            123,
+            234,
+            345,
+            456,
+            567,
+            678,
+            789,
+            1234,
+            2345,
+            3456,
+            4567,
+            5678,
+            6789,
+            12345,
+            23456,
+            34567,
+            45678,
+            56789,
+            123456,
+            234567,
+            345678,
+            456789,
+            1234567,
+            2345678,
+            3456789,
+            12345678,
+            23456789,
+            123456789,
+        ]
+        ans = []
+        
+        for num in s:
+            if num >= low and num <= high:
+                ans.append(num)
+        
+        return ans

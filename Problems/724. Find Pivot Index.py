499c68b7a6b81996925b778412702fd0c3a91875
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-18 19:09:32 +0600

    Easy one pass

--- Problems/724. Find Pivot Index.py
+++ Problems/724. Find Pivot Index.py
@@ -2,23 +2,13 @@ from typing import *
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        if not nums:
-            return -1
+        S = sum(nums)
+        left_sum = 0
         
-        def get_at(arr, index):
-            if index < 0 or index >= len(arr):
-                return 0
-            
-            return arr[index]
-        
-        N = len(nums)
-        prefix = []
-        
-        for i in range(N):
-            prefix.append(get_at(prefix, i-1) + nums[i])
-        
-        for i in range(N):
-            if get_at(prefix, i-1) == get_at(prefix, N-1) - get_at(prefix, i):
+        for i, val in enumerate(nums):
+            if left_sum == S - left_sum - nums[i]:
                 return i
+            
+            left_sum += val
         
         return -1

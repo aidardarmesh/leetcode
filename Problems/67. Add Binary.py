cf94e0cf64b438b84f1ab7a783dec3df47160a5c
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-18 21:24:54 +0600

    Recalled binary add

--- Problems/67. Add Binary.py
+++ Problems/67. Add Binary.py
@@ -2,7 +2,15 @@ from typing import *
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2) + int(b,2))[2:]
+        def add(a, b):
+            while b:
+                a, b = a ^ b, (a & b) << 1
+            
+            return a
+        
+        sum_ = add(int(a, 2), int(b, 2))
+        
+        return bin(sum_)[2:]
 
 s = Solution()
 

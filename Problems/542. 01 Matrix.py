8de89e5018c7ad0354c601a2bfaac6a3e85a6a28
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-18 14:15:35 +0600

    More optimized BFS on dist

--- Problems/542. 01 Matrix.py
+++ Problems/542. 01 Matrix.py
@@ -2,33 +2,24 @@ from typing import *
 
 class Solution:
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        queue = []
-        visited = set()
         N = len(matrix)
         M = len(matrix[0])
+        dist = [[float('inf') for j in range(M)] for i in range(N)]
+        queue = []
         
         for i in range(N):
             for j in range(M):
                 if matrix[i][j] == 0:
+                    dist[i][j] = 0
                     queue.append((i,j))
         
         while queue:
             i, j = queue.pop(0)
             
-            if (i,j) in visited:
-                continue
-                
-            visited.add((i,j))
-            min_ = float('inf')
-            
-            for ni,nj in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
+            for ni, nj in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
                 if 0 <= ni < N and 0 <= nj < M:
-                    if not (ni,nj) in visited:
+                    if dist[i][j] + 1 < dist[ni][nj]:
+                        dist[ni][nj] = dist[i][j] + 1
                         queue.append((ni,nj))
-                    else:
-                        min_ = min(min_, matrix[ni][nj])
-            
-            if matrix[i][j] != 0:
-                matrix[i][j] = min_ + 1
         
-        return matrix
+        return dist

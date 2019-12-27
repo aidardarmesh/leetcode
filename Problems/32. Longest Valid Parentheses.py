ba4ad332b70fedfbc309b1ab6fbbf8735103a458
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-27 14:45:33 +0600

    DP

--- Problems/32. Longest Valid Parentheses.py
+++ Problems/32. Longest Valid Parentheses.py
@@ -2,31 +2,19 @@ from typing import *
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        dp = [0 for _ in range(n)]
         max_ = 0
-        left = right = 0
-        
-        for i in range(len(s)):
-            if s[i] == '(':
-                left += 1
-            else:
-                right += 1
-            
-            if left == right:
-                max_ = max(max_, left*2)
-            elif right > left:
-                left = right = 0
-        
-        left = right = 0
-        
-        for i in range(len(s)-1, -1, -1):
+
+        for i in range(1, n):
             if s[i] == ')':
-                right += 1
-            else:
-                left += 1
-            
-            if left == right:
-                max_ = max(max_, right*2)
-            elif left > right:
-                left = right = 0
+                if s[i-1] == '(':
+                    dp[i] = 2 + (dp[i-2] if i-2 >= 0 else 0)
+                else:
+                    nei = i-dp[i-1]-1
+
+                    dp[i] = dp[i-1] + 2 + (dp[nei-1] if nei-1 >= 0 else 0)
+
+                max_ = max(max_, dp[i])
         
         return max_

93904ce5d5929352aac2c3c35f14fd7373a60ead
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-12-29 21:31:03 +0600

    DP

---
+++ Problems/1143. Longest Common Subsequence.py
@@ -0,0 +1,20 @@
+from typing import *
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if not text1 or not text2:
+            return 0
+        
+        n = len(text1)
+        m = len(text2)
+        dp = [[0 for j in range(m+1)] for i in range(n+1)]
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[n][m]
+

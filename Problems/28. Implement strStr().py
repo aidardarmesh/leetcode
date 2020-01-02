5ac634a103a338faee07c6a50e54b6cd162e7e2c
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-02 23:11:22 +0600

    Rolling hash

--- Problems/28. Implement strStr().py
+++ Problems/28. Implement strStr().py
@@ -2,10 +2,33 @@ from typing import *
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle == "":
+        n, m = len(haystack), len(needle)
+        
+        if m > n:
+            return -1
+        
+        a = 26
+        modulus = 0x7FFFFFFF
+        A = pow(a, m, modulus)
+        h = h_curr = 0
+        
+        def to_int(s, i):
+            return ord(s[i]) - ord('a')
+        
+        for i in range(m):
+            h = (h*a + to_int(needle, i)) % modulus
+            h_curr = (h_curr*a + to_int(haystack, i)) % modulus
+            
+        if h == h_curr:
             return 0
-
-        return haystack.find(needle)
+        
+        for i in range(m, n):
+            h_curr = (h_curr*a - to_int(haystack, i-m)*A + to_int(haystack, i)) % modulus
+            
+            if h == h_curr:
+                return i-m+1
+        
+        return -1
 
 s = Solution()
 

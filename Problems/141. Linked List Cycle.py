a5f50fd26f811748147bf7afdb85eb311dadd4ee
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-05 20:30:53 +0600

    Sentinel makes code so smooth

--- Problems/141. Linked List Cycle.py
+++ Problems/141. Linked List Cycle.py
@@ -1,43 +1,23 @@
 from typing import *
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        visited = []
+        sent = ListNode(0)
+        sent.next = head
 
-        while head != None:
-            if head in visited:
+        slow, fast = sent, head
+
+        while fast and fast.next:
+            if slow == fast:
                 return True
-            else:
-                visited.append(head)
             
-            head = head.next
-
+            slow = slow.next
+            fast = fast.next.next
+        
         return False
-
-s = Solution()
-
-# [3,2,0,-4]
-# nodeA = ListNode(3)
-# nodeB = ListNode(2)
-# nodeC = ListNode(0)
-# nodeD = ListNode(-4)
-# nodeA.next = nodeB
-# nodeB.next = nodeC
-# nodeC.next = nodeD
-# nodeD.next = nodeB
-
-# print(s.hasCycle(nodeA))
-
-# [1,2]
-nodeA = ListNode(1)
-nodeB = ListNode(2)
-nodeA.next = nodeB
-nodeB.next = nodeA
-
-print(s.hasCycle(nodeA))

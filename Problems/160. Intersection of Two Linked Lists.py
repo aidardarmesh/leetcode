82fcf11cb07a8130ae1396e326845570611eec94
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-05 22:01:23 +0600

    Same start. Correct

--- Problems/160. Intersection of Two Linked Lists.py
+++ Problems/160. Intersection of Two Linked Lists.py
@@ -6,16 +6,30 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        curA, curB = headA, headB
+        nA, nB, curA, curB = 0, 0, headA, headB
         
         while curA:
-            while curB:
-                if curA == curB:
-                    return curA
-                
-                curB = curB.next
-            
             curA = curA.next
-            curB = headB
+            nA += 1
+        
+        while curB:
+            curB = curB.next
+            nB += 1
+        
+        curA, curB = headA, headB
+        
+        if nA < nB:
+            for _ in range(nB-nA):
+                curB = curB.next
+        elif nA > nB:
+            for _ in range(nA-nB):
+                curA = curA.next
+        
+        while curA and curB:
+            if curA == curB:
+                return curA
+
+            curA = curA.next
+            curB = curB.next
         
         return None

a07fb0845ad6a30293e2aaaeeb07ef26ab4aebe3
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-06 12:15:12 +0600

    Used Doubly-Linked List, because its node does not need other references to be deleted. Sentinels helped

--- Problems/146. LRU Cache.py
+++ Problems/146. LRU Cache.py
@@ -1,36 +1,64 @@
 from typing import *
-from collections import OrderedDict
 
-class LRUCache(OrderedDict):
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
 
     def __init__(self, capacity: int):
+        self.size = 0
+        # key: node
+        self.cache = {}
         self.capacity = capacity
+        self.head = ListNode(None, None)
+        self.tail = ListNode(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _remove_node(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+    
+    def _add_node(self, node):
+        neigh = self.head.next
+        node.next = neigh
+        node.prev = self.head
+        self.head.next = node
+        neigh.prev = node
+    
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+    
+    def _pop_last(self):
+        last = self.tail.prev
+        self._remove_node(last)
+        del self.cache[last.key]
 
     def get(self, key: int) -> int:
-        if key not in self:
+        if not key in self.cache:
             return -1
         
-        self.move_to_end(key)
-
-        return self[key]
+        node = self.cache[key]
+        self._move_to_head(node)
+        
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self:
-            self.move_to_end(key)
-        
-        self[key] = value
-
-        if len(self) > self.capacity:
-            self.popitem(last=False)
-
-cache = LRUCache(2)
-
-cache.put(1, 1)
-cache.put(2, 2)
-assert cache.get(1) == 1
-cache.put(3, 3)
-assert cache.get(2) == -1
-cache.put(4, 4)
-assert cache.get(1) == -1
-assert cache.get(3) == 3
-assert cache.get(4) == 4
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._move_to_head(node)
+        else:
+            self.size += 1
+            node = ListNode(key, value)
+            self.cache[key] = node
+            self._add_node(node)
+            
+            if self.size > self.capacity:
+                self._pop_last()
+                self.size -= 1

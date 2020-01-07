eab9fd1946038f32af189ecc6055882f9013b8b6
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-07 22:02:07 +0600

    Math

---
+++ Problems/390. Elimination Game.py
@@ -0,0 +1,18 @@
+from typing import *
+
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        head = 1
+        step = 1
+        left = True
+        
+        while n > 1:
+            if left or n % 2 == 1:
+                head += step
+            
+            
+            step *= 2
+            n //= 2
+            left = not left
+        
+        return head

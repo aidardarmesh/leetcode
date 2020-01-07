bcad1d87f1ae2b482933be7ee05afd73cfc11040
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-07 17:36:56 +0600

    ArrayList and HashTable

--- Problems/380. Insert Delete GetRandom O(1).py
+++ Problems/380. Insert Delete GetRandom O(1).py
@@ -1,5 +1,4 @@
 from typing import *
-import random
 
 class RandomizedSet:
 
@@ -7,41 +6,40 @@ class RandomizedSet:
         """
         Initialize your data structure here.
         """
-        self.set = set()
+        self.map = {}
+        self.data = []
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val not in self.set:
-            self.set.add(val)
-
-            return True
+        if val in self.map:
+            return False
         
-        return False
+        self.map[val] = len(self.data)
+        self.data.append(val)
+        
+        return True
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.set:
-            self.set.discard(val)
-            
-            return True
+        if not val in self.map:
+            return False
         
-        return False
+        last_val, idx = self.data[-1], self.map[val]
+        self.data[idx], self.map[last_val] = last_val, idx
+        
+        self.data.pop()
+        del self.map[val]
+        
+        return True
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return random.sample(self.set, 1)[0]
+        rand_idx = random.randrange(len(self.data))
 
-r = RandomizedSet()
-print(r.insert(1)) # True
-print(r.remove(2)) # False
-print(r.insert(2)) # True
-print(r.getRandom())
-print(r.remove(1)) # True
-print(r.insert(2)) # False
-print(r.getRandom()) # 2
+        return self.data[rand_idx]

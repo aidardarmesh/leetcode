2af0401e307038bf243151f7ddc29b3882d8598d
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-08 20:15:22 +0600

    Pure Binary Search Solution

--- Problems/34. Find First and Last Position of Element in Sorted Array.py
+++ Problems/34. Find First and Last Position of Element in Sorted Array.py
@@ -2,30 +2,38 @@ from typing import *
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def bin_search(left, right, target):
+        def leftmost():
+            left, right = 0, len(nums)-1
+            candidate = -1
+            
             while left <= right:
-                mid = (left+right) // 2
+                mid = (left + right) // 2
                 
                 if nums[mid] == target:
-                    return mid
+                    candidate = mid
+                    right = mid - 1
                 elif nums[mid] < target:
                     left = mid + 1
                 else:
                     right = mid - 1
+            
+            return candidate
         
-            return -1
+        def rightmost():
+            left, right = 0, len(nums)-1
+            candidate = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    candidate = mid
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return candidate
         
-        pos = bin_search(0, len(nums)-1, target)
-        
-        if pos == -1:
-            return [-1, -1]
-        
-        left, right = pos, pos
-        
-        while left >= 0 and nums[left] == target:
-            left -= 1
-        
-        while right <= len(nums)-1 and nums[right] == target:
-            right += 1
-        
-        return [left+1, right-1]
+        return [leftmost(), rightmost()]

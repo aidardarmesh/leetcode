bafffdf626afc862e6cc3d333f9524220324dd4d
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-08 11:50:46 +0600

    Two pointers. Close to 3-Sum problem

--- Problems/611. Valid Triangle Number.py
+++ Problems/611. Valid Triangle Number.py
@@ -6,21 +6,14 @@ class Solution:
         nums.sort()
         cnt = 0
         
-        def bin_search(left, right, x):
-            while left <= right:
-                mid = (left+right) // 2
-                
-                if nums[mid] < x:
-                    left = mid+1
+        for i in range(N-1, 1, -1):
+            left, right = 0, i-1
+
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    cnt += right-left
+                    right -= 1
                 else:
-                    right = mid-1
-            
-            return left
-        
-        for i in range(N):
-            for j in range(i+1, N):
-                k = bin_search(j+1, N-1, nums[i]+nums[j])
-                
-                cnt += (k-1) - (j+1) + 1
+                    left += 1
         
         return cnt

e489a5eb8066570770cf0652e887a8e1c4fa5a32
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-09 20:45:23 +0600

    Sentinel

--- Problems/2. Add Two Numbers.py
+++ Problems/2. Add Two Numbers.py
@@ -1,76 +1,33 @@
 from typing import *
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = dummy = ListNode(-1)
-        int_ = 0
-        sum_ = 0
-
-        while l1 != None or l2 != None:
-            sum_ = int_
-
-            if l1 != None:
+        sent = ListNode(-1)
+        prev = sent
+        factor = 0
+        
+        while l1 or l2:
+            sum_ = factor
+            
+            if l1:
                 sum_ += l1.val
                 l1 = l1.next
             
-            if l2 != None:
+            if l2:
                 sum_ += l2.val
                 l2 = l2.next
-                
-            rem = sum_ % 10
-            int_ = sum_ // 10
-            dummy.next = ListNode(rem)
-            dummy = dummy.next
-
-        if int_ != 0:
-            dummy.next = ListNode(int_)
-            dummy = dummy.next
+            
+            prev.next = ListNode(sum_ % 10)
+            prev = prev.next
+            factor = sum_ // 10
         
-        return head.next
-
-    def print(self, head: ListNode):
-        while head != None:
-            print(head.val, end=" ")
-            head = head.next
+        if factor:
+            prev.next = ListNode(factor)
         
-        print()
-
-s = Solution()
-
-# nodeA_0 = ListNode(0)
-
-# nodeB_0 = ListNode(0)
-# nodeB_1 = ListNode(1)
-# nodeB_0.next = nodeB_1
-
-# s.print(s.addTwoNumbers(nodeA_0, nodeB_0))
-
-# nodeA_2 = ListNode(2)
-# nodeA_4 = ListNode(4)
-# nodeA_2.next = nodeA_4
-
-# nodeB_8 = ListNode(8)
-# nodeB_5 = ListNode(5)
-# nodeB_8.next = nodeB_5
-
-# s.print(s.addTwoNumbers(nodeA_2, nodeB_8))
-
-# nodeA_2 = ListNode(2)
-# nodeA_4 = ListNode(4)
-# nodeA_3 = ListNode(3)
-# nodeA_2.next = nodeA_4
-# nodeA_4.next = nodeA_3
-
-# nodeB_5 = ListNode(5)
-# nodeB_6 = ListNode(6)
-# nodeB_4 = ListNode(4)
-# nodeB_5.next = nodeB_6
-# nodeB_6.next = nodeB_4
-
-# s.print(s.addTwoNumbers(nodeA_2, nodeB_5))
+        return sent.next

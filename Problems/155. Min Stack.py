c43bebf36d710b2ea5985b2917f33161c9bf96cf
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-09 12:56:54 +0600

    Simpler-better. Same as Max Stack

--- Problems/155. Min Stack.py
+++ Problems/155. Min Stack.py
@@ -4,39 +4,23 @@ class MinStack:
 
     def __init__(self):
         self.data = []
-        self.size = 0
-    
-    def is_empty(self):
-        return self.size == 0
 
     def push(self, x: int) -> None:
-        min_ = self.getMin()
-        
-        if min_ is None or x < min_:
-            min_ = x
-        
-        self.data.append((x, min_))
-        self.size += 1
+        if len(self.data) > 0:
+            min_ = self.getMin()
+            
+            self.data.append((x, min(x, min_)))
+        else:
+            self.data.append((x, x))
 
     def pop(self) -> None:
-        if not self.is_empty():
-            self.data.pop()
-            self.size -= 1
+        if len(self.data) > 0:
+            return self.data.pop()[0]
 
     def top(self) -> int:
-        if not self.is_empty():
-            return self.data[self.size-1][0]
+        if len(self.data) > 0:
+            return self.data[-1][0]
 
     def getMin(self) -> int:
-        if not self.is_empty():
-            return self.data[self.size-1][1]
-
-
-m = MinStack()
-m.push(-2)
-m.push(0)
-m.push(-3)
-print(m.getMin())
-m.pop()
-print(m.top())
-print(m.getMin())
+        if len(self.data) > 0:
+            return self.data[-1][1]

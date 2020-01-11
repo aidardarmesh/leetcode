82136f6f444d393148a60afaff9c8d3ff5c6d20b
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-11 23:29:06 +0600

    Resolved

--- Problems/250. Count Univalue Subtrees.py
+++ Problems/250. Count Univalue Subtrees.py
@@ -1,47 +1,32 @@
 from typing import *
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def countUnivalSubtrees(self, root: TreeNode) -> int:
-        self.count = 0
-        self.is_uni(root)
-
-        return self.count
-    
-    def is_uni(self, root: TreeNode):
         if not root:
-            return False
-
-        if not root.left and not root.right:
-            self.count += 1
+            return 0
         
-            return True
+        self.cnt = 0
         
-        is_uni = True
-
-        if root.left:
-            is_uni = self.is_uni(root.left) and root.left.val == root.val
+        def uni(root):
+            is_uni = True
+            
+            if root.left:
+                is_uni = uni(root.left) and is_uni  and root.val == root.left.val
+            
+            if root.right:
+                is_uni = uni(root.right) and is_uni and root.val == root.right.val
+            
+            self.cnt += is_uni
+            
+            return is_uni
         
-        if root.right:
-            is_uni = self.is_uni(root.right) and root.right.val == root.val and is_uni
+        uni(root)
         
-        self.count += is_uni
-
-        return is_uni
-
-s = Solution()
-
-a = TreeNode(5)
-a.right = TreeNode(5)
-a.right.right = TreeNode(5)
-a.left = TreeNode(1)
-a.left.left = TreeNode(5)
-a.left.right = TreeNode(5)
-
-assert s.countUnivalSubtrees(a) == 4
+        return self.cnt

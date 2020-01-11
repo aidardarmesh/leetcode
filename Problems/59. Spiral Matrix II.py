10997b70f1a4ea1eb9befe9df9d0062b52aade3e
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-11 14:09:50 +0600

    Better Spiral

--- Problems/59. Spiral Matrix II.py
+++ Problems/59. Spiral Matrix II.py
@@ -2,31 +2,25 @@ from typing import *
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        m = [[0 for j in range(n)] for i in range(n)]
+        m = [[0]*n for _ in range(n)]
+        seen = [[False]*n for _ in range(n)]
         counter = 1
-
-        for i in range(0, round(n/2)):
-            for j in range(i, n-1-i):
-                m[i][j] = counter
-                counter += 1
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        dir_idx = 0
+        r, c = 0, 0
         
-            for j in range(i, n-1-i):
-                m[j][n-1-i] = counter
+        for i in range(n):
+            for j in range(n):
+                m[r][c] = counter
+                seen[r][c] = True
                 counter += 1
-            
-            for j in range(n-1-i, i, -1):
-                m[n-1-i][j] = counter
-                counter += 1
-            
-            for j in range(n-1-i, i, -1):
-                m[j][i] = counter
-                counter += 1
-
-        if n % 2 == 1:
-            m[n//2][n//2] = counter
+                
+                nr, nc = r + dirs[dir_idx][0], c + dirs[dir_idx][1]
+                
+                if 0 <= nr < n and 0 <= nc < n and not seen[nr][nc]:
+                    r, c = nr, nc
+                else:
+                    dir_idx = (dir_idx+1) % 4
+                    r, c = r + dirs[dir_idx][0], c + dirs[dir_idx][1]
         
         return m
-
-s = Solution()
-
-print(s.generateMatrix(4))

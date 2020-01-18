aae22982892f5dc31b8b1aa46ff3562e3811aaa1
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-18 20:02:13 +0600

    DP

--- Problems/139. Word Break.py
+++ Problems/139. Word Break.py
@@ -4,18 +4,12 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         words = set(wordDict)
-        dp = [False for _ in range(n+1)]
+        dp = [False]*(n+1)
         dp[0] = True
         
-        for i in range(1, n+1):
-            for j in range(i):
-                if dp[j] and s[j:i] in words:
-                    dp[i] = True
+        for j in range(1, n+1):
+            for i in range(j):
+                if dp[i] and s[i:j] in words:
+                    dp[j] = True
         
         return dp[n]
-
-s = Solution()
-
-print(s.wordBreak("applepenapple", ["apple", "pen"]))
-print(s.wordBreak("catsandog", ["cats", "cat", "dog", "sand", "and"]))
-print(s.wordBreak("leetcode", ["leet", "code"]))

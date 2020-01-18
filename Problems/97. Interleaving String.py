5ac6025b8d2f7c854b32f0e84cb826fca6a483f7
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-19 00:01:20 +0600

    DP 2D matrix

--- Problems/97. Interleaving String.py
+++ Problems/97. Interleaving String.py
@@ -2,21 +2,24 @@ from typing import *
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        def interleave(s1, i, s2, j, res, s3, k):
-            if res == s3 and i == len(s1) and j == len(s2):
-                return True
-            
-            if len(res) > 0 and k < len(s3) and res[-1] != s3[k]:
-                return False
-            
-            ans = False
-            
-            if i < len(s1):
-                ans |= interleave(s1, i+1, s2, j, res+s1[i], s3, k+1)
-            
-            if j < len(s2):
-                ans |= interleave(s1, i, s2, j+1, res+s2[j], s3, k+1)
-            
-            return ans
+        if len(s3) != len(s1) + len(s2):
+            return False
         
-        return interleave(s1, 0, s2, 0, '', s3, -1)
+        n = len(s1)
+        m = len(s2)
+
+        dp = [[False] * (m+1) for _ in range(n+1)]
+
+        for i in range(n+1):
+            for j in range(m+1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[j-1] and s2[j-1] == s3[i+j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1] and s1[i-1] == s3[i+j-1]
+                else:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1] or dp[i][j-1] and s2[j-1] == s3[i+j-1]
+        
+        return dp[n][m]
+                

9778bd0347945fbed449312032c056f28365574d
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-19 22:26:34 +0600

    Clockwise and vice-versa

--- Problems/48. Rotate Image.py
+++ Problems/48. Rotate Image.py
@@ -2,14 +2,36 @@ from typing import *
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
+        # anti-clockwise
+        # 1. reverse first
+        # 2. transpose
+        def rotate_left():
+            n = len(matrix)
 
-        # transpose
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            # reverse
+            for i in range(n):
+                for j in range(n//2):
+                    matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+            
+            # transpose
+            for i in range(n):
+                for j in range(i, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        # reverse
-        for i in range(n):
-            for j in range(n//2):
-                matrix[i][j], matrix[n-1-j] = matrix[n-1-j], matrix[i][j]
+        # clockwise
+        # 1. transpose
+        # 2. reverse
+        def rotate_right():
+            n = len(matrix)
+
+            # transpose
+            for i in range(n):
+                for j in range(i, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+            # reverse
+            for i in range(n):
+                for j in range(n//2):
+                    matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+
+        rotate_right()

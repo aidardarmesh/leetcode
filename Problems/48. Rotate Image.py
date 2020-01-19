7a49de2dd820d6e26cf05f43b380a5e99841f9c5
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-19 22:11:18 +0600

    Rotated to 90 degree clockwise by transposition and reversing

--- Problems/48. Rotate Image.py
+++ Problems/48. Rotate Image.py
@@ -2,42 +2,14 @@ from typing import *
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         n = len(matrix)
 
+        # transpose
         for i in range(n):
             for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
+        # reverse
         for i in range(n):
-            matrix[i].reverse()
-
-s = Solution()
-a = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-b = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
-]
-c = [
-    [1,2],
-    [3,4]
-]
-d = [
-    [1]
-]
-s.rotate(a)
-s.rotate(b)
-s.rotate(c)
-s.rotate(d)
-print(a)
-print(b)
-print(c)
-print(d)
+            for j in range(n//2):
+                matrix[i][j], matrix[n-1-j] = matrix[n-1-j], matrix[i][j]

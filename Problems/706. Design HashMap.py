73a873e347fcd8874d98bcde7ba911ce5d38bc74
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-23 21:01:42 +0600

    Added _index

--- Problems/706. Design HashMap.py
+++ Problems/706. Design HashMap.py
@@ -1,6 +1,12 @@
 from typing import *
 
 
+class Pair:
+        
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+
 class MyHashMap:
 
     def __init__(self):
@@ -12,40 +18,53 @@ class MyHashMap:
     
     def _hash(self, key):
         return key % self.n
+    
+    def _index(self, key):
+        idx = self._hash(key)
+        bucket = self.buckets[idx]
+        
+        for i, pair in enumerate(bucket):
+            if pair.key == key:
+                return bucket, i
+        
+        return bucket, -1
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        idx = self._hash(key)
-
-        for elem in self.buckets[idx]:
-            if elem[0] == key:
-                elem[1] = value
-                return
+        bucket, idx = self._index(key)
         
-        self.buckets[idx].append([key, value])
+        if idx >= 0:
+            bucket[idx].val = value
+            return
+        
+        bucket.append(Pair(key, value))
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        idx = self._hash(key)
+        bucket, idx = self._index(key)
 
-        for elem in self.buckets[idx]:
-            if elem[0] == key:
-                return elem[1]
+        if idx < 0:
+            return -1
         
-        return -1
+        return bucket[idx].val
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        idx = self._hash(key)
+        bucket, idx = self._index(key)
+        
+        if idx < 0:
+            return
+        
+        bucket.pop(idx)
 
-        for i, elem in enumerate(self.buckets[idx]):
-            if elem[0] == key:
-                self.buckets[idx].pop(i)
-                return
-
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)

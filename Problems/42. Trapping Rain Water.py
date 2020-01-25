ab5da18b89e79f97b707b54b3fb770130d24e480
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-01-25 19:54:05 +0600

    DP

--- Problems/42. Trapping Rain Water.py
+++ Problems/42. Trapping Rain Water.py
@@ -2,23 +2,20 @@ from typing import *
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right, left_max, right_max = 0, len(height)-1, 0, 0
+        N = len(height)
+        left, right = [0]*N, [0]*N
+        left_max = right_max = 0
         ans = 0
         
-        while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
-                else:
-                    ans += left_max - height[left]
-                
-                left += 1
-            else:
-                if height[right] >= right_max:
-                    right_max = height[right]
-                else:
-                    ans += right_max - height[right]
-                
-                right -= 1
+        for i in range(N):
+            left_max = max(left_max, height[i])
+            left[i] = left_max
+        
+        for i in range(N-1, -1, -1):
+            right_max = max(right_max, height[i])
+            right[i] = right_max
+        
+        for i in range(N):
+            ans += min(left[i], right[i]) - height[i]
         
         return ans

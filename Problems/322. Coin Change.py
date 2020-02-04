2df27f0af2eadeb53e4d61964c639e6e7562af79
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-04 21:09:08 +0600

    Rewritten

--- Problems/322. Coin Change.py
+++ Problems/322. Coin Change.py
@@ -5,13 +5,9 @@ class Solution:
         dp = [float('inf')]*(amount+1)
         dp[0] = 0
         
-        def change(target, coin):
-            if target >= coin:
-                return dp[target-coin]
-            
-            return float('inf')
-        
         for target in range(1, amount+1):
-            dp[target] = min([change(target, coin) for coin in coins]) + 1
+            for coin in coins:
+                if target >= coin:
+                    dp[target] = min(dp[target], dp[target-coin]+1)
         
         return dp[amount] if dp[amount] != float('inf') else -1

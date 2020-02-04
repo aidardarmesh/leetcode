1661ca5f538d8996e6d81cc3ec45453a078b7ba2
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-04 21:02:48 +0600

    DP

--- Problems/322. Coin Change.py
+++ Problems/322. Coin Change.py
@@ -2,16 +2,16 @@ from typing import *
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {i:float('inf') for i in range(amount+1)}
-        dp[amount] = 0
+        dp = [float('inf')]*(amount+1)
+        dp[0] = 0
         
-        def changes(val):
-            if val < 0 or val > amount:
-                return float('inf')
+        def change(target, coin):
+            if target >= coin:
+                return dp[target-coin]
             
-            return dp[val]
+            return float('inf')
         
-        for i in range(amount-1, -1, -1):
-            dp[i] = min([changes(i+coin) for coin in coins]) + 1
+        for target in range(1, amount+1):
+            dp[target] = min([change(target, coin) for coin in coins]) + 1
         
-        return dp[0] if dp[0] != float('inf') else -1
+        return dp[amount] if dp[amount] != float('inf') else -1

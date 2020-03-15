333fb35a247c4506f7c4a1068441fce392fdf00f
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-03-15 22:49:43 +0600

    Another solution using sorted

--- Problems/692. Top K Frequent Words.py
+++ Problems/692. Top K Frequent Words.py
@@ -2,10 +2,4 @@ from typing import *
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        cnt = collections.Counter(words)
-        heap = []
-
-        for word, num in cnt.items():
-            heapq.heappush(heap, (-num, word))
-        
-        return [heapq.heappop(heap) for _ in range(k)]
+        return sorted(collections.Counter(words), key=lambda x: (-cnt[x], x))[:k]

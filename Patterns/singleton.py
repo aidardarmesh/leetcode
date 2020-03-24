16b5eb6adfa2c57696ba557942f0e68ea0d54f1c
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-03-24 23:08:46 +0600

    Singleton

---
+++ Patterns/singleton.py
@@ -0,0 +1,21 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            raise Exception("This is Singleton!")
+
+        Singleton.__instance = self
+
+    @staticmethod
+    def get_instance():
+        if not Singleton.__instance:
+            Singleton()
+        
+        return Singleton.__instance
+
+s = Singleton()
+print(s)
+print(Singleton.get_instance() == s)
+s = Singleton()
+print("Exception didn't raise")

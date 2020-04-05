9681ecbe8085b34e7eb4d3f0aadbecb345124eed
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-04-05 22:57:29 +0600

    String

---
+++ Problems/856. Score of Parentheses.py
@@ -0,0 +1,16 @@
+from typing import *
+
+class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        ans = depth = 0
+        
+        for i, c in enumerate(S):
+            if c == '(':
+                depth += 1
+            else:
+                depth -= 1
+                
+                if S[i-1] == '(':
+                    ans += 1 << depth
+        
+        return ans

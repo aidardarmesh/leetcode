a33c7fc752c060ad19d50539cefc703ec6d34294
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-04-09 19:56:02 +0600

    Several mirror swaps

--- Problems/189. Rotate Array.py
+++ Problems/189. Rotate Array.py
@@ -2,8 +2,21 @@ from typing import *
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
-            nums.insert(0, nums.pop())
+        if not k:
+            return nums
+        
+        n = len(nums)
+        k = k % n
+        
+        def mirror(start, end):
+            while start <= end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        mirror(0, n-1)
+        mirror(0, k-1)
+        mirror(k, n-1)
 
 s = Solution()
 

4e5e8586a62bbf00bce69035761970192c6c287f
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-04-11 17:20:05 +0600

    Resolved

--- Problems/91. Decode Ways.py
+++ Problems/91. Decode Ways.py
@@ -2,9 +2,6 @@ from typing import *
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s:
-            return 0
-        
         n = len(s)
         dp = [0 for _ in range(n+1)]
         dp[n] = 1
@@ -12,11 +9,17 @@ class Solution:
         for i in range(n-1, -1, -1):
             if s[i] == '0':
                 continue
+            elif s[i] == '1':
+                dp[i] = dp[i+1]
                 
-            dp[i] = dp[i+1]
-            
-            if i+1 < n:
-                if s[i] == '1' or (s[i] == '2' and s[i+1] <= '6'):
+                if i+2 <= n:
                     dp[i] += dp[i+2]
+            elif s[i] == '2':
+                dp[i] = dp[i+1]
+                
+                if i+2 <= n and s[i+1] <= '6':
+                    dp[i] += dp[i+2]
+            else:
+                dp[i] = dp[i+1]
         
         return dp[0]

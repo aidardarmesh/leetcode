cca43686281c4eeb5f81945de0c3bed5799e1f9b
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-04-12 20:37:30 +0600

    DP (Prefix Sum)

--- Problems/724. Find Pivot Index.py
+++ Problems/724. Find Pivot Index.py
@@ -2,13 +2,23 @@ from typing import *
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        S = sum(nums)
-        left_sum = 0
+        if not nums:
+            return -1
         
-        for i, val in enumerate(nums):
-            if left_sum == S - left_sum - nums[i]:
-                return i
+        n = len(nums)
+        prefix = [0] * n
+        
+        def get_at(arr, idx):
+            if idx < 0 or idx >= len(arr):
+                return 0
             
-            left_sum += val
+            return arr[idx]
+        
+        for i in range(n):
+            prefix[i] = get_at(prefix, i-1) + nums[i]
+        
+        for i in range(n):
+            if get_at(prefix, i-1) == get_at(prefix, n-1) - get_at(prefix, i):
+                return i
         
         return -1

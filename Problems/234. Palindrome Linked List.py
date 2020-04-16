776a0d0f66c1efc71c04a73e3d7f133917a02369
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-04-16 11:12:00 +0600

    Two Pointers

--- Problems/234. Palindrome Linked List.py
+++ Problems/234. Palindrome Linked List.py
@@ -8,19 +8,37 @@ class ListNode:
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        stack = []
-        queue = []
-
-        while head != None:
-            stack.append(head.val)
-            queue.append(head.val)
-
-            head = head.next
+        n, node = 0, head
         
-        while len(stack) > 0:
-            if stack.pop() != queue.pop(0):
+        while node:
+            n += 1
+            node = node.next
+        
+        node = head
+        half = n//2
+        
+        for _ in range(half-1):
+            next_ = node.next
+            node.next = next_.next
+            next_.next = head
+            head = next_
+        
+        fast = head
+        half = half+1 if n%2 == 1 else half
+        
+        for _ in range(half):
+            fast = fast.next
+        
+        slow = head
+        half = half-1 if n%2 == 1 else half
+        
+        for _ in range(half):
+            if slow.val != fast.val:
                 return False
-
+            
+            slow = slow.next
+            fast = fast.next
+        
         return True
     
 s = Solution()
